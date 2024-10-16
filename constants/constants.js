e97b24272ee7353102858cc30785883bbaf2453d
smallpony222 <smallpony64@gmail.com>
2024-10-16 07:29:14 -0500

    create constants

---
+++ constants/constants.js
@@ -0,0 +1,147 @@
+export const NAME = 'Alfredo Garza';
+
+export const DESIGNATION = `Senior Software Engineer \n Full Stack Engineer`;
+
+export const DETAILS = {
+  Residence: 'TX, U.S.',
+  City: 'Raymondville',
+  Age: '33',
+};
+
+export const SKILLS = [
+  {
+    title: 'JavaScript & TypeScript (Frontend)',
+    level: '90%',
+  },
+  {
+    title: 'Java( Spring Boot )',
+    level: '85%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Python( Django, FLASK, FASTAPI )',
+    level: '70%',
+  },
+  {
+    title: 'C#( ASP.NET Core )',
+    level: '80%',
+  },
+  {
+    title : 'Database( SQL & NOSQL )',
+    level : "90%"
+  },
+  {
+    title : 'Cloud Service( AWS, AZURE, Netlify, HeroKu )',
+    level : "65%"
+  }
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'ReactJS',
+  'NextJS',
+  'VueJS',
+  'TypeScript',
+  'NodeJS',
+  'Express.js',
+  'Redux',
+  'Webpack',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'PostgreSQL',
+  'Git',
+  'GitHub',
+  'Jira',
+  'Figma',
+];
+
+export const technologies = {
+  Languages: [
+    'JavaScript',
+    'TypeScript',
+    'C#',
+    'Python',
+    'Java',
+    'C',
+    'C++'
+  ],
+  Frontend: [
+    'JavaScript',
+    'ReactJS',
+    'NextJS',
+    'VueJS',
+    'TypeScript',
+    'Redux',
+    'Webpack',
+    'CSS',
+    'SCSS',
+    'TailwindCSS',
+    'Ant Design',
+    'Material UI',
+    'Next UI'
+  ],
+  Backend: [
+    'NodeJS',
+    'Express.js',
+    'Python',
+    'Django',
+    'Flask',
+    'FastAPI',
+    'Java',
+    'Spring Boot',
+    'C#',
+    'ASP.NET Core',
+    'REST API'
+  ],
+  Databases: [
+    'MongoDB',
+    'MySQL',
+    'PostgreSQL'
+  ],
+  Cloud_DevOps: [
+    'AWS Lambda',
+    'Amplify',
+    'GCP',
+    'AWS',
+    'Azure',
+    'Netlify',
+    'Heroku',
+    'Docker',
+    'Kubernetes',
+    'Jenkins'
+  ],
+  MessageBrokers: [
+    'RabbitMQ',
+    'Kafka',
+    'Redis'
+  ],
+  Tools: [
+    'Git',
+    'GitHub',
+    'Jira',
+    'Figma',
+    'D3'
+  ]
+};
+
+
+export const CONTACTS = {
+  EMAIL: 'alfredogarza@gmail.com',
+  PHONE: '+14696383235',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/osamajavaid',
+  LINKEDIN: 'https://www.linkedin.com/in/iosamajavaid/',
+  TWITTER: 'https://twitter.com/iosamajavaid',
+  FACEBOOK: 'https://www.facebook.com/iosamajavaid',
+};
